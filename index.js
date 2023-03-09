22ab1f6fb1726433ef4bdaebccdcc3e3e2e8ab98
HoKangInfo <hokang.info@gmail.com>
2023-03-09 10:11:52 +0800

    add, dd

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+export { default as dd } from './lib/dd.js'
+
+export { default as print } from './lib/print.js'
+
+export { default as printf } from './lib/printf.js'
+
+export { default as isString } from './lib/isString.js'
