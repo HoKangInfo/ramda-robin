6d390e466f54ec51371048549ae4ad9d9a5296cc
HoKangInfo <hokang.info@gmail.com>
2023-05-12 12:02:58 +0800

    add help

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 
 export { default as dd } from './lib/dd.js'
 
+export { default as d } from './lib/d.js'
+
 export { default as print } from './lib/print.js'
+
+/**
+ * @example
+ *    p('hello world')
+ */
+export { default as p } from './lib/print.js'
+
+export { default as pp } from './lib/pp.js'
 
 export { default as printf } from './lib/printf.js'
 
